5faefbd36ffa9f31d8a391999e0e5e4143195c66
velmyk <velmyk@gmail.com>
2016-11-30 10:55:51 +0200

    feat(employee form): add validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,21 @@ import { Employee } from '../models/employee.model';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  private languages = [
+  languages = [
     "english",
     "spanish",
     "france"
   ];
-  model = new Employee('Darla', 'Smith', true, 'w2', 'english');
+
+  model = new Employee('Darla', 'Smith', true, 'w2', 'default');
+
+  hasPrimaryLanguageError = false;
+  
+  validatePrimaryLanguage(value) {
+    if (value === 'default') {
+      this.hasPrimaryLanguageError = true;
+    } else {
+      this.hasPrimaryLanguageError = false;
+    }
+  }
 }
